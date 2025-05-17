a26a701953733342d40c17cf944225b359508496
BrianWeiHaoMa <brianmaytc@gmail.com>
2025-05-17 17:06:41 -0400

    create utils.py

--- main.py
+++ main.py
@@ -1,17 +1,73 @@
-import sounddevice as sd
-import soundfile as sf
-import tempfile
-import moviepy as mp
+"""A simple example of recording from speakers ('What you hear') using the WASAPI loopback device"""
 
-video_path = r"examples/steins_gate_shorter.mkv"
+from _spinner_helper import Spinner
+# Spinner is a helper class that is in the same examples folder.
+# It is optional, you can safely delete the code associated with it.
 
-# Extract audio and save to a temporary WAV file
-video = mp.VideoFileClip(video_path)
-with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
-    audio_path = tmpfile.name
-    video.audio.write_audiofile(audio_path, codec='pcm_s16le')
+import pyaudiowpatch as pyaudio
+import time
+import wave
 
-# Read and play audio
-data, samplerate = sf.read(audio_path)
-sd.play(data, samplerate)
-sd.wait()
+DURATION = 5.0
+CHUNK_SIZE = 512
+
+filename = "loopback_record.wav"
+    
+if __name__ == "__main__":
+    with pyaudio.PyAudio() as p:
+        """
+        Create PyAudio instance via context manager.
+        Spinner is a helper class, for `pretty` output
+        """
+        try:
+            # Get default WASAPI info
+            wasapi_info = p.get_host_api_info_by_type(pyaudio.paWASAPI)
+        except OSError:
+            print("Looks like WASAPI is not available on the system. Exiting...")
+            exit()
+        
+        # Get default WASAPI speakers
+        default_speakers = p.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
+        
+        if not default_speakers["isLoopbackDevice"]:
+            for loopback in p.get_loopback_device_info_generator():
+                """
+                Try to find loopback device with same name(and [Loopback suffix]).
+                Unfortunately, this is the most adequate way at the moment.
+                """
+                if default_speakers["name"] in loopback["name"]:
+                    default_speakers = loopback
+                    break
+            else:
+                print("Default loopback output device not found.\n\nRun `python -m pyaudiowpatch` to check available devices.\nExiting...\n")
+                exit()
+                
+        print(f"Recording from: ({default_speakers['index']}){default_speakers['name']}")
+        
+        wave_file = wave.open(filename, 'wb')
+        wave_file.setnchannels(default_speakers["maxInputChannels"])
+        wave_file.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+        wave_file.setframerate(int(default_speakers["defaultSampleRate"]))
+        
+        def callback(in_data, frame_count, time_info, status):
+            """Write frames and return PA flag"""
+            wave_file.writeframes(in_data)
+            return (in_data, pyaudio.paContinue)
+        
+        with p.open(format=pyaudio.paInt16,
+                channels=default_speakers["maxInputChannels"],
+                rate=int(default_speakers["defaultSampleRate"]),
+                frames_per_buffer=CHUNK_SIZE,
+                input=True,
+                input_device_index=default_speakers["index"],
+                stream_callback=callback
+        ) as stream:
+            """
+            Opena PA stream via context manager.
+            After leaving the context, everything will
+            be correctly closed(Stream, PyAudio manager)            
+            """
+            print(f"The next {DURATION} seconds will be written to {filename}")
+            time.sleep(DURATION) # Blocking execution while playing
+        
+        wave_file.close()
---
+++ utils.py
@@ -0,0 +1,145 @@
+import io 
+import pyaudiowpatch as pyaudio
+import time
+import wave
+import tempfile
+
+from _secrets import OPEN_AI_API_KEY
+import openai
+
+
+client = openai.OpenAI(api_key=OPEN_AI_API_KEY)
+
+
+def record_audio(
+        filename="output.wav",
+        duration=5.0, 
+        chunk_size=512, 
+):
+    """
+    Record audio from the PC's main output device and save it to a file.
+    """
+    with pyaudio.PyAudio() as p:
+        """
+        Create PyAudio instance via context manager.
+        """
+        try:
+            # Get default WASAPI info
+            wasapi_info = p.get_host_api_info_by_type(pyaudio.paWASAPI)
+        except OSError:
+            print("Looks like WASAPI is not available on the system. Exiting...")
+            exit()
+        
+        # Get default WASAPI speakers
+        default_speakers = p.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
+        
+        if not default_speakers["isLoopbackDevice"]:
+            for loopback in p.get_loopback_device_info_generator():
+                """
+                Try to find loopback device with same name(and [Loopback suffix]).
+                Unfortunately, this is the most adequate way at the moment.
+                """
+                if default_speakers["name"] in loopback["name"]:
+                    default_speakers = loopback
+                    break
+            else:
+                print("Default loopback output device not found.\n\nRun `python -m pyaudiowpatch` to check available devices.\nExiting...\n")
+                exit()
+                
+        print(f"Recording from: ({default_speakers['index']}){default_speakers['name']}")
+        
+        wave_file = wave.open(filename, 'wb')
+        wave_file.setnchannels(default_speakers["maxInputChannels"])
+        wave_file.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+        wave_file.setframerate(int(default_speakers["defaultSampleRate"]))
+        
+        def callback(in_data, frame_count, time_info, status):
+            """Write frames and return PA flag"""
+            wave_file.writeframes(in_data)
+            return (in_data, pyaudio.paContinue)
+        
+        with p.open(format=pyaudio.paInt16,
+                channels=default_speakers["maxInputChannels"],
+                rate=int(default_speakers["defaultSampleRate"]),
+                frames_per_buffer=chunk_size,
+                input=True,
+                input_device_index=default_speakers["index"],
+                stream_callback=callback
+        ) as stream:
+            """
+            Opena PA stream via context manager.
+            After leaving the context, everything will
+            be correctly closed(Stream, PyAudio manager)            
+            """
+            print(f"The next {duration} seconds will be written to {filename}")
+            while True:
+                time.sleep(duration) # Blocking execution while playing
+        
+        # wave_file.close()
+        
+
+def record_audio_into_tmp_file(
+        duration=5.0, 
+        chunk_size=512, 
+):
+    """
+    Record audio from the PC's main output device and save it to a temporary file.
+    """
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp_file:
+        filename = tmp_file.name
+        print(f"Temporary file created: {filename}")
+        record_audio(filename, duration, chunk_size)
+        
+    return filename
+
+
+def transcribe_audio(file_path, language="ja"):
+    with open(file_path, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            model="gpt-4o-transcribe", 
+            file=audio_file,
+            language=language,
+            include="punctuations",
+            temperature=0.3,
+        )
+    
+    return transcription.text
+
+
+def translate_text(text, source_language="ja", target_language="en"):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        temperature=0.3,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a translator for dialogues from games who will "
+                           f"translate from {source_language} to {target_language}.",
+            },
+            {
+                "role": "user",
+                "content": text,
+            },
+        ]
+    )
+    
+    return completion.choices[0].message.content
+
+
+def generate_audio(
+        text,
+        voice="alloy", 
+        instructions="You are a character in some game.", 
+):
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        voice=voice,
+        input=text,
+        instructions=instructions,
+        response_format="wav",
+    ) as response:
+        audio_bytes = io.BytesIO(response.read())
+    
+    return audio_bytes
+
+
