3607fc4ca6d931badc611c0f34071a75ffc6205c
BrianWeiHaoMa <brianmaytc@gmail.com>
2025-05-17 22:59:03 -0400

    update readme, add controls.py for language options

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ from utils import (
     translate_text,
     generate_audio,
 )
+from controls import (
+    SOURCE_LANGUAGE,
+    TARGET_LANGUAGE,
+)
 
 
 def run(stop_event: Event = Event()):
     while not stop_event.is_set():
         file_path = record_audio_into_tmp_file(stop_event=stop_event)
 
-        text = transcribe_audio(file_path)
+        text = transcribe_audio(file_path, language=SOURCE_LANGUAGE)
         print(text, end="\n\n")
 
         if text.strip() == "" or stop_event.is_set():
             return
 
-        translated_text = translate_text(text)
+        translated_text = translate_text(text, source_language=SOURCE_LANGUAGE, target_language=TARGET_LANGUAGE)
         print(translated_text, end="\n\n")
 
         if translated_text.strip() == "" or stop_event.is_set():
