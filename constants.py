839a3a7c436181a25e2042d44568b9926f4bc65b
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2025-05-19 01:49:47 -0700

    Extract constants into new constants.py

---
+++ constants.py
@@ -0,0 +1,74 @@
+API_KEY_FILE = "_secrets.py"
+
+ASCII_LOGO = r"""
+::::::::::: :::::::::      :::     ::::    :::  ::::::::  :::            ::: ::::::::::: ::::::::  :::::::::
+    :+:     :+:    :+:   :+: :+:   :+:+:   :+: :+:    :+: :+:          :+: :+:   :+:    :+:    :+: :+:    :+:
+    +:+     +:+    +:+  +:+   +:+  :+:+:+  +:+ +:+        +:+         +:+   +:+  +:+    +:+    +:+ +:+    +:+
+    +#+     +#++:++#:  +#++:++#++: +#+ +:+ +#+ +#++:++#++ +#+        +#++:++#++: +#+    +#+    +:+ +#++:++#:
+    +#+     +#+    +#+ +#+     +#+ +#+  +#+#+#        +#+ +#+        +#+     +#+ +#+    +#+    +#+ +#+    +#+
+    #+#     #+#    #+# #+#     #+# #+#   #+#+# #+#    #+# #+#        #+#     #+# #+#    #+#    #+# #+#    #+#
+    ###     ###    ### ###     ### ###    ####  ########  ########## ###     ### ###     ########  ###    ###
+"""
+
+LANGUAGES = [
+    ("sq", "Albanian"),
+    ("am", "Amharic"),
+    ("ar", "Arabic"),
+    ("hy", "Armenian"),
+    ("bn", "Bengali"),
+    ("bs", "Bosnian"),
+    ("bg", "Bulgarian"),
+    ("my", "Burmese"),
+    ("ca", "Catalan"),
+    ("zh", "Chinese"),
+    ("hr", "Croatian"),
+    ("cs", "Czech"),
+    ("da", "Danish"),
+    ("nl", "Dutch"),
+    ("en", "English"),
+    ("et", "Estonian"),
+    ("fi", "Finnish"),
+    ("fr", "French"),
+    ("ka", "Georgian"),
+    ("de", "German"),
+    ("el", "Greek"),
+    ("gu", "Gujarati"),
+    ("hi", "Hindi"),
+    ("hu", "Hungarian"),
+    ("is", "Icelandic"),
+    ("id", "Indonesian"),
+    ("it", "Italian"),
+    ("ja", "Japanese"),
+    ("kn", "Kannada"),
+    ("kk", "Kazakh"),
+    ("ko", "Korean"),
+    ("lv", "Latvian"),
+    ("lt", "Lithuanian"),
+    ("mk", "Macedonian"),
+    ("ms", "Malay"),
+    ("ml", "Malayalam"),
+    ("mr", "Marathi"),
+    ("mn", "Mongolian"),
+    ("no", "Norwegian"),
+    ("fa", "Persian"),
+    ("pl", "Polish"),
+    ("pt", "Portuguese"),
+    ("pa", "Punjabi"),
+    ("ro", "Romanian"),
+    ("ru", "Russian"),
+    ("sr", "Serbian"),
+    ("sk", "Slovak"),
+    ("sl", "Slovenian"),
+    ("so", "Somali"),
+    ("es", "Spanish"),
+    ("sw", "Swahili"),
+    ("sv", "Swedish"),
+    ("tl", "Tagalog"),
+    ("ta", "Tamil"),
+    ("te", "Telugu"),
+    ("th", "Thai"),
+    ("tr", "Turkish"),
+    ("uk", "Ukrainian"),
+    ("ur", "Urdu"),
+    ("vi", "Vietnamese"),
+]
